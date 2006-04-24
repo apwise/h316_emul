cc18299c5e120849dfd599f47dc0446725bbfd6b
Adrian Wise <adrian@adrianwise.co.uk>
2006-04-24 10:53:57 +0000

    Moving towards proper auto* environment
    git-svn-id: svn://kore/repository/history/trunk@106 d6098cfc-5bd4-0310-8f2b-90d8aa983419

--- version.h
+++ version.h
@@ -1,6 +1,5 @@
 #define NAME "h16"
-#define VERSION "1.4.x"
-#define COPYRIGHT "Copyright (c) 1998, 1999, 2000, 2001, 2004, 2005 Adrian Wise"
-#define DATE "date..."
-//#include "obj/date.h"
+#define VERSION "1.4.1"
+#define COPYRIGHT "Copyright (c) 1998, 1999, 2000, 2001, 2004, 2005, 2006 Adrian Wise"
+#include "date.h"
 
