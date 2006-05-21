8f7c7d578e1b5cbe7eef484251381324031faffb
Adrian Wise <adrian@adrianwise.co.uk>
2006-05-21 11:40:29 +0000

    git-svn-id: svn://kore/repository/history/trunk@124 d6098cfc-5bd4-0310-8f2b-90d8aa983419

---
+++ utils/h16-asctotty.c
@@ -0,0 +1,133 @@
+/*
+ * Honeywell Series 16 emulator - convert a ASCII to teletype forced-parity
+ *
+ * Copyright (C) 1997, 2006  Adrian Wise
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307 USA
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+#define EOM  0203
+#define XOFF 0223
+#define RUBOUT 0377
+
+#define USAGE "usage: %s [-h|--help] [-c] <ASCII filename> <TTY filename>\n"
+
+int main(int argc, char **argv)
+{
+  FILE *fpi, *fpo;
+  int c, d;
+  int usage = 0;
+  int help = 0;
+  int a = 1;
+  int add_eom = 0;
+
+  while (argv[a])
+    {
+      if ((strncmp(argv[a], "-h", 2)==0) || (strncmp(argv[a], "--h", 3)==0))
+	{
+	  help = 1;
+	  break;
+	}
+      else if (strcmp(argv[a], "-c")==0)
+	{
+	  add_eom = 1;
+	  a++;
+	}
+      else
+	break;
+    }
+  
+  if (help)
+    {
+      printf(USAGE, argv[0]);
+      printf("Options:\n");
+      printf("-h|--help  Print this help and exit\n");
+      printf("-c         Add <EOM> at end of file\n");
+      printf("\n");
+      printf("           Lower-case letters are replaced by upper-case.\n");
+      printf("           Newline replaced by CR-LF,\n");
+      printf("           other control codes are deleted.\n");
+      printf("           Most significant bit of all characters forced to 1.\n");
+      exit(0);
+    }
+
+  if ((argc-a) != 2)
+    usage = 1;
+
+  if (usage)
+    {
+      fprintf(stderr, USAGE, argv[0]);
+      exit(1);
+    }
+
+  fpi = fopen(argv[a], "r");
+  if (!fpi)
+    {
+      fprintf(stderr, "Could not open <%s> for reading\n",
+	      argv[a]);
+      exit(1);
+    }
+
+  a++;
+  fpo = fopen(argv[a], "wb");
+  if (!fpi)
+    {
+      fprintf(stderr, "Could not open <%s> for writing\n",
+	      argv[a]);
+      exit(1);
+    }
+
+  c = getc(fpi);
+
+  while (c != EOF)
+    {
+      c = c & 0x7f;  /* loose the top bit */
+      d = 0;
+			
+      if (c < 0x20)
+	{
+	  /* Only control code allowed is newline
+	     and this is replaced by CR (LF added later) */
+	  if (c == '\n')
+	    d = 0x0d;
+	}
+      else if ((c > 0x60) && (c < 0x7f))
+	d = c - ('a'-'A'); /* fold lower case to upper */
+      else
+	d = c; /* Printing capitals are unaffected */
+      
+      if (d > 0)
+	{
+	  putc((d | 0x80), fpo);
+	  if (d == 0x0d)
+	    putc( (0x0a | 0x80), fpo);
+	}
+      c = getc(fpi);
+    }
+	
+  if (add_eom)
+    putc(EOM, fpo);
+
+  fclose(fpi);
+  fclose(fpo);
+
+  exit(0);
+}
+
+
---
+++ utils/h16-ttytoasc.c
@@ -0,0 +1,115 @@
+/*
+ * Honeywell Series 16 emulator - convert a teletype output to ASCII
+ *
+ * Copyright (C) 1997, 2006  Adrian Wise
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307 USA
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define USAGE "usage: %s [-h|--help] <TTY filename> <ASCII filename>\n"
+
+int main(int argc, char **argv)
+{
+  int a = 1;
+  int help = 0;
+  int usage = 0;
+  FILE *fpi, *fpo;
+  int c, d;
+
+  while (argv[a])
+    {
+      if ((strncmp(argv[a], "-h", 2)==0) || (strncmp(argv[a], "--h", 3)==0))
+	{
+	  help = 1;
+	  break;
+	}
+      else
+	break;
+    }
+  
+  if (help)
+    {
+      printf(USAGE, argv[0]);
+      printf("Options:\n");
+      printf("-h|--help  Print this help and exit\n");
+      printf("\n");
+      printf("           CR-LF replaced by newline,\n");
+      printf("           other control codes are deleted.\n");
+      printf("           Most significant bit of all characters forced to 0.\n");
+      exit(0);
+    }
+
+  if ((argc-a) != 2)
+    usage = 1;
+
+  if (usage)
+    {
+      fprintf(stderr, USAGE, argv[0]);
+      exit(1);
+    }
+
+  fpi = fopen(argv[a], "rb");
+  if (!fpi)
+    {
+      fprintf(stderr, "Could not open <%s> for reading\n",
+	      argv[a]);
+      exit(1);
+    }
+  a++;
+
+  fpo = fopen(argv[a], "w");
+  if (!fpi)
+    {
+      fprintf(stderr, "Could not open <%s> for writing\n",
+	      argv[a]);
+      exit(1);
+    }
+
+  c = getc(fpi);
+
+  while (c != EOF)
+    {
+      c = c & 0x7f;  /* loose the top bit */
+      d = 0;
+
+      if (c < 0x20)
+	{
+	  /* replace LF by newline (let local OS where this
+	     code is running worry about how to represent \n */
+	  if (c == 0x0a)
+	    d = '\n';
+
+	  /* CR and other ctrl chars are ignored */
+	}
+      else
+	d = c;
+      
+      if (d > 0)
+	putc(d, fpo);
+      c = getc(fpi);
+    }
+
+  fclose(fpi);
+  fclose(fpo);
+
+  exit(0);
+}
+
+
