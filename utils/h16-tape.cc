6d3d8ce50ea412529774b63d1a87b3f31dcd36d7
Adrian Wise <adrian@adrianwise.co.uk>
2006-09-25 09:45:20 +0000

    Added h16-tape (renames slst)
    git-svn-id: svn://kore/repository/history/trunk@182 d6098cfc-5bd4-0310-8f2b-90d8aa983419

---
+++ utils/h16-tape.cc
@@ -0,0 +1,443 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <iostream>
+
+#include "instr.hh"
+
+#define TAPE_INITIAL_BUFFER_SIZE 256
+
+class Tape
+{
+public:
+  Tape();
+  ~Tape();
+
+private:
+  unsigned char *buffer;
+  unsigned long buffer_size;
+  unsigned long buffer_used;
+};
+
+Tape::Tape()
+  : buffer_size(TAPE_INITIAL_BUFFER_SIZE),
+    buffer_used(0)
+{
+  buffer = new unsigned char [buffer_size];
+}
+
+Tape::~Tape()
+{
+  delete [] buffer;
+}
+
+static InstrTable instr_table;
+
+// {{{ static int gc(FILE *fp)
+
+static int gc(FILE *fp)
+{
+  int c = getc(fp);
+
+  if (c==EOF)
+    {
+      fprintf(stderr, "Unexpected EOF\n");
+      exit(1);
+    }
+  return c&0xff;
+}
+
+// }}}
+// {{{ static unsigned short read_8_8(FILE *fp)
+
+static unsigned short read_8_8(FILE *fp)
+{
+  return (gc(fp) << 8) | gc(fp);
+}
+
+// }}}
+// {{{ static void skip_leader(FILE *fp)
+
+static void skip_leader(FILE *fp)
+{
+  int c=0;
+
+  while (c==0)
+    c = gc(fp);
+
+  ungetc(c, fp);
+}
+
+// }}}
+// {{{ static int read_8_8_block(FILE *fp, unsigned short *buffer)
+
+static int read_8_8_block(FILE *fp, unsigned short *buffer)
+{
+  int c=0;
+  int i=0;
+  unsigned short n;
+
+  skip_leader(fp);
+
+  while ((n=read_8_8(fp)) != 0)
+    {
+      buffer[i] = n;
+      /*printf("%06o %06o\n", i, buffer[i]);*/
+      i++;
+    }
+  return i;
+}
+
+// }}}
+// {{{ static void read_expected(FILE *fp, int expect)
+
+static void read_expected(FILE *fp, int expect)
+{
+  int c;
+  c = gc(fp);
+
+  if (c!=expect)
+    {
+      fprintf(stderr, "Expected %03o read %03o\n", expect, c);
+      //exit(1);
+    }
+  /*printf("read %03o OK\n", c);*/
+} 
+
+// }}}
+// {{{ static int translate(int c, bool &xof)
+
+static int translate(int c, bool &xof)
+{
+  int n;
+
+  if (c == 0223)
+    {
+      fprintf(stderr, "XOF mid-word\n");
+      xof = 1;
+      //exit(1);
+    }
+
+   switch(c & 0x7f)
+    {
+    case 0177: n = 023 | (c & 0x80); break;
+    case 0176: n = 021 | (c & 0x80); break;
+    case 0175: n = 012 | (c & 0x80); break;
+    case 0174: n = 005 | (c & 0x80); break;
+    default: n = c;
+    }
+
+  if (n & 0x60)
+    {
+      fprintf(stderr, "Channels 6 or 7 punched (%03o)\n", n);
+      //exit(1);
+    }
+
+  n = ((n&0x80)>>2) | (n & 0x1f);
+  /*printf("translate %03o to %03o\n", c, n);*/
+
+  return n;
+}
+
+// }}}
+// {{{ static unsigned short read_silent(FILE *fp, bool &zero_flag, bool &xof)
+
+static unsigned short read_silent(FILE *fp, bool &zero_flag, bool &xof)
+{
+  int c;
+  unsigned short n;
+
+  c = gc(fp);
+  if (c == 0223)
+    {
+      xof = 1;
+      return 0;
+    }
+  else
+    xof = 0;
+
+  if (c & 0x80)
+    {
+      zero_flag = 1;
+      c &= 0x7f;
+    }
+  else
+    zero_flag = 0;
+
+  c = translate(c, xof);
+  if (c & 0x10)
+    {
+      fprintf(stderr, "Channel 5 punched in 4-bit character\n");
+      //exit(1);
+    }
+  n = (c << 12);
+
+  n |= translate(gc(fp), xof) << 6;
+  n |= translate(gc(fp), xof);
+
+  return n;
+}
+
+// }}}
+
+static unsigned short core_low = 0xffff;
+static unsigned short core_high = 0;
+
+// {{{ static int read_silent_block(FILE *fp, int find_start,
+
+static int read_silent_block(FILE *fp, int find_start,
+			     unsigned short *core,
+			     unsigned short bc)
+{
+  unsigned short addr, a, n;
+  signed short zeros = 0;
+  bool zero_flag, xof;
+  unsigned short checksum = bc;
+  unsigned short old_checksum = checksum;
+  int wc = 0;
+  int i, c;
+  skip_leader(fp);
+
+  c = gc(fp);
+
+  if (c==0223)
+    return 0;
+
+  if (find_start)
+    {
+      while (c != 0201)
+	c = gc(fp);
+    }
+  else if (c!=0201)
+    {
+      fprintf(stderr, "Expected %03o read %03o\n", 0201, c);
+      exit(1);
+    }
+
+  addr = read_silent(fp, zero_flag, xof);
+
+  checksum ^= addr;
+  checksum = ((checksum & 1) << 15) |
+    ((checksum >> 1) & 0x7fff);
+  addr++;
+  a = addr;
+
+  if (zero_flag)
+    {
+      fprintf(stderr, "Zero flag on address\n");
+      exit(1);
+    }
+
+  /*printf("addr = %06o\n", addr);*/
+
+  n = read_silent(fp, zero_flag, xof);
+  while (!xof)
+    {
+      if (zero_flag)
+        {
+          i = n | (-1<<16);
+          while (i<0)
+            {
+              core[a++] = 0;
+              i++;
+              wc++;
+            }
+        }
+      else
+        {
+          core[a++] = n;
+          wc++;
+        }
+      old_checksum = checksum;
+      checksum ^= n;
+      checksum = ((checksum & 1) << 15) |
+        ((checksum >> 1) & 0x7fff);
+      
+      n = read_silent(fp, zero_flag, xof);
+    }
+  
+  read_expected(fp, 0377);
+  
+  checksum = core[--a];  /* from tape */
+  old_checksum ^= wc; /* calculated here */
+  
+  printf("Block %03d %06o to %06o %06o %s\n",
+   bc, addr, a-1, (a-addr),
+   (checksum != old_checksum) ? "Bad checksum\n":"");
+
+  if (checksum != old_checksum)
+    printf("Expected: %06o Actual: %06o XOR: %06o\n",
+	   old_checksum, checksum, (old_checksum ^ checksum));
+
+  if (addr < core_low) core_low = addr;
+  if ((a-1) > core_high) core_high = (a-1);
+
+  return 1;
+}
+
+// }}}
+// {{{ static unsigned char asr_char(unsigned char c)
+
+static unsigned char asr_char(unsigned char c)
+{
+  if ((c >= 0240) && (c <= 0337))
+    return c & 0x7f;
+  else if ((c==212) || (c==215))
+    return ' ';
+  else
+    return 0;
+}
+
+// }}}
+// {{{ static char *printable(unsigned short instr)
+
+static char *printable(unsigned short instr)
+{
+  static char temp[5];
+  
+  temp[1] = (char) asr_char((instr >> 8) & 0xff);
+  temp[2] = (char) asr_char(instr & 0xff);
+
+  if (temp[1] && temp[2])
+      temp[0] = temp[3] = '"';
+  else
+    temp[0] = '\0';
+  temp[4]= '\0';
+
+  return temp;
+}
+
+// }}}
+
+static bool verilog = false;
+
+// {{{ static void dissassemble_block(unsigned short *block, int size, int addr)
+
+static void dissassemble_block(unsigned short *block, int size, int addr)
+{
+  int i;
+
+  if (verilog)
+    printf("@%04x\n", addr & 0xffff);
+  else
+    printf("{\n");
+
+  for (i=0; i<size;i++)
+    {
+      if (verilog)
+	printf("%04x /* %-35s %-4s */\n",
+	       block[i],
+	       instr_table.disassemble((i+addr), block[i], false),
+	       printable(block[i]));
+      else
+	printf("  0%06o%c /* %-35s %-4s */\n",
+	       block[i],
+	       ((i ==(size-1)) ? ' ' : ','),
+	       instr_table.disassemble((i+addr), block[i], false),
+	       printable(block[i]));
+
+    }
+
+  if (!verilog)
+    printf("}\n");
+}
+
+// }}}
+
+#define K64 (1 << 16)
+#define K32 (1 << 15)
+#define K16 (1 << 14)
+
+int main(int argc, char **argv)
+{
+  FILE *fp;
+  int c;
+  int bootstrap_length;
+  unsigned short bootstrap[256];
+  int loader_length, loader_addr;
+  unsigned short loader[256];
+  int palap = 0;
+  int expect_at_least;
+
+  unsigned short *core;
+  int i;
+  int bc=0;
+
+  //debug = (struct INSTR **) malloc(sizeof(struct INSTR *) * K64);
+  //dispatch = (void (proc::**)(unsigned short))
+  //malloc(sizeof(void (proc::*)(unsigned short)) * K64);
+
+  //instr_table.build_instr_tables();
+
+  core = (unsigned short *) malloc(sizeof(unsigned short) * K32);
+  for (i=0; i<K32; i++)
+    core[i] = 0;
+
+  int a = 1;
+  bool parsing_args = true;
+
+  while (parsing_args && (a < argc))
+    {
+      if (strcmp(argv[a], "-v")==0)
+	{
+	  verilog = true;
+	  a++;
+	}
+      else
+	parsing_args = false;
+    }
+
+  if (a != (argc-1))
+    {
+      fprintf(stderr, "usage: %s [-v] <filename>\n",
+       argv[0]);
+      exit(1);
+    }
+
+  fp = fopen(argv[a], "rb");
+  if (!fp)
+    {
+      fprintf(stderr, "Could not open <%s> for reading\n",
+        argv[a]);
+      exit(1);
+    }
+
+  skip_leader(fp);
+  read_expected(fp, 020);
+  bootstrap_length = read_8_8_block(fp, bootstrap);
+  printf("bootstrap_length = %d\n", bootstrap_length);
+  dissassemble_block(bootstrap, bootstrap_length, 020);
+
+  // assume some form of palap
+  palap = 1;
+  loader_addr = bootstrap[054 - 020]+2;
+  expect_at_least = 040;
+  loader_length = 0;
+
+  while (loader_length < expect_at_least )
+    {
+      loader_length = read_8_8_block(fp, loader + loader_length);
+      loader[loader_length++] = 0;
+    }
+  loader_length --;
+
+  printf("loader_length = %d\n", loader_length);
+  dissassemble_block(loader, loader_length, loader_addr);
+
+  read_expected(fp, 0);
+  read_expected(fp, 0223);
+  read_expected(fp, 0377);
+  //dissassemble_block(loader, loader_length, 0602);
+
+  do{}
+    while(read_silent_block(fp, 1, core, bc++));
+
+  dissassemble_block(core+core_low, ((core_high-core_low)+1), core_low);
+
+  fclose(fp);
+  
+  exit(0);
+}
+
+
