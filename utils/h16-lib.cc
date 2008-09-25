4251ac6201311de8a473095c92aa6fdd6c2e3988
Adrian Wise <adrian@adrianwise.co.uk>
2008-09-25 16:56:31 +0000

    Bump version number
    git-svn-id: svn://kore/repository/history/trunk@566 d6098cfc-5bd4-0310-8f2b-90d8aa983419

--- utils/h16-lib.cc
+++ utils/h16-lib.cc
@@ -1,3 +1,27 @@
+/* Manipulate library files
+ *
+ * Copyright (C) 2008  Adrian Wise
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307 USA
+ *
+ *
+ * At the moment all this can do is concatenate object files
+ * into a library, removing spurious Fortran-IV End-Of-Job blocks
+ */
+
 
 #include <iostream>
 #include <istream>
