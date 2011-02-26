5f6f7cc68a2b06f961259bba7b63a4e5df43dea8
Adrian Wise <adrian@adrianwise.co.uk>
2011-02-26 13:35:55 +0000

    Bump version to 1.5.0
    git-svn-id: svn://kore/repository/history/trunk@731 d6098cfc-5bd4-0310-8f2b-90d8aa983419

--- proc.hh
+++ proc.hh
@@ -1,5 +1,5 @@
 /* Honeywell Series 16 emulator
- * Copyright (C) 1997, 1998, 2005  Adrian Wise
+ * Copyright (C) 1997, 1998, 2005, 2010, 2011  Adrian Wise
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
