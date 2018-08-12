b8aa6144b2e78d0a0dcf060deb44f1e23063a828
Adrian Wise <adrian@adrianwise.co.uk>
2018-08-12 22:19:57 +0100

    Add junit test result reporting

--- tests/ab16cct4/ab16cct4.sh
+++ tests/ab16cct4/ab16cct4.sh
@@ -1,3 +1,35 @@
-#! /bin/sh
-../../install/bin/h16 -t ab16cct4.txt > logfile.txt
-diff logfile.txt expected.txt
+#! /bin/bash
+
+workspace="$( cd `dirname $0`/../..; pwd )"
+
+. ../scripts/jshutest.inc
+
+jshu_pkgname=h316_emul
+BUILDDIR=${workspace}
+
+ab16cct4_Test()
+{
+  ../../install/bin/h16 -t ab16cct4.txt > logfile.txt
+  if diff logfile.txt expected.txt; then
+      return ${jshuPASS}
+  else
+      return ${jshuFAIL}
+  fi
+}
+
+##############################################################
+# main
+##############################################################
+# initialize testsuite
+jshuInit
+
+# run unit tests in this script
+jshuRunTests
+
+# result summary
+jshuFinalize
+
+echo Done.
+echo
+let tot=failed+errors
+exit $tot
