f59b20e2ef147b5e4fda2fd5ae531851df98653f
Adrian Wise <adrian@adrianwise.co.uk>
2019-05-19 08:58:55 +0100

    Copyright notices

--- wxwindows/papertape.cc
+++ wxwindows/papertape.cc
@@ -1,12 +1,24 @@
-#include <wx/tglbtn.h>
-#include <wx/stattext.h>
-#include <wx/dcclient.h>
-
+/* Graphical representation of papertape
+ *
+ * Copyright (c) 2004, 2005, 2019  Adrian Wise
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307 USA
+ *
+ */
 #include <cmath>
-#include <iostream>
-
-#include <list>
-
 #include "papertape.hh"
 
 #define TIMER_PERIOD 100 // update every tenth of a second
