7ec6b9d57769066e8d4911828bf4d790461f45e5
Adrian Wise <adrian@adrianwise.co.uk>
2019-05-26 11:54:11 +0100

    Add missing file

---
+++ wxwindows/simpleport.hh
@@ -0,0 +1,12 @@
+#ifndef _SIMPLE_PORT_
+#define _SIMPLE_PORT_
+
+class SimplePort
+{
+public:
+  SimplePort(){}
+  virtual ~SimplePort(){}
+  virtual void Process(unsigned char ch, int source) = 0;
+};
+
+#endif // _SIMPLE_PORT_
