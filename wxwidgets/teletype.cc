cfb89ef9e86cde6e351b2932cc50e775a3562ec2
Adrian Wise <adrian@adrianwise.co.uk>
2020-05-19 14:04:40 +0100

    Partial move to wxGTK3.0-gtk3.0, serialport AsyncInput

--- wxwidgets/teletype.cc
+++ wxwidgets/teletype.cc
@@ -21,10 +21,13 @@
 #include "asr_widget.hh"
 
 #include <wx/graphics.h>
+#include <wx/config.h>
 
 #include "serialport.h"
 
 #include "teletype_logo_128.xpm"
+
+#define ROOT_CONFIG "teletypeApp"
 
 class MyApp: public wxApp
 {
@@ -37,7 +40,8 @@ public:
   
   MyFrame(const wxString &title, const wxPoint &pos = wxDefaultPosition,
           const wxSize &size = wxDefaultSize);
-
+  virtual ~MyFrame();
+  
   void OnQuit(wxCommandEvent& event);
   void OnAbout(wxCommandEvent& event);
   void OnClose(wxCloseEvent &event);
@@ -47,11 +51,12 @@ public:
   void OnKeyUp(wxKeyEvent& event);
   void OnChar(wxKeyEvent& event);
 
-  virtual void Process(unsigned char ch, int source);
+  virtual void Process(unsigned char ch, int source=0);
 
 private:
   static const int CHARACTER_TIMER_ID = 0;
 
+  /*
   enum class Sources {
     Keyboard,
     SpecialFunctionKey,
@@ -59,12 +64,18 @@ private:
     AnswerBack,
     Line
   };
-
+  */
+  
   //unsigned int ppi;
   wxBoxSizer *top_sizer;
   AsrWidget  *asr_widget;
 
   //unsigned int GetPPI();
+
+  wxSerialPort *port;
+  wxSerialPort::Config config;
+  //wxSerialPort::SignalEventSet *ses;
+  wxSerialPort::AsyncInput *asi;
   
   DECLARE_EVENT_TABLE()
 };
@@ -98,6 +109,8 @@ bool MyApp::OnInit()
 void MyFrame::OnClose(wxCloseEvent &event)
 {
   wxString message;
+
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
   
   if ( event.CanVeto() && asr_widget->Unsaved(message) ) {
     if ( wxMessageBox(message,
@@ -107,6 +120,23 @@ void MyFrame::OnClose(wxCloseEvent &event)
       return;
     }
   }
+  
+  /*
+  if (ses) {
+    delete ses;
+    ses = 0;
+  }
+  */
+  if (asi) {
+    asi->Stop();
+    delete asi;
+    asi = 0;
+  }
+  if (port) {
+    delete port;
+    port = 0;
+  }
+  
   event.Skip(); // Let the default handler do its thing
 }
 
@@ -132,7 +162,10 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
   : wxFrame((wxFrame *)NULL, -1, title, pos, size)
     //, ppi(GetPPI())
   , top_sizer(new wxBoxSizer(wxHORIZONTAL))
-  , asr_widget(new AsrWidget(this))
+  , asr_widget(new AsrWidget(this, this))
+  , port(0)
+    //, ses(0)
+  , asi(0)
 {
   /*
   wxMenu *menuFile = new wxMenu;
@@ -160,51 +193,90 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
   Refresh();
 
 #if 1
-  wxSerialPortArray portArray;
+  //wxSerialPortArray portArray;
+  wxSerialPort::portArray_t portArray;
   wxSerialPort::Return r = wxSerialPort::ListPorts(portArray);
 
   std::cout << "ListPorts() returned " << static_cast<int>(r) << std::endl;
 
   unsigned int i;
-  for (i=0; i<portArray.GetCount(); i++) {
+  //for (i=0; i<portArray.GetCount(); i++) {
+  for (i=0; i<portArray.size(); i++) {
     std::cout << portArray[i].GetPortName() << std::endl;
   }
 
-  wxSerialPort::Config config(r);
-  std::cout << "Config() returned " << static_cast<int>(r) << std::endl;
+  if (config.Allocated() != wxSerialPort::OK)
+    std::cerr << "config not allocated" << std::endl;
 
-  wxSerialPort port(portArray[1]);
-  if (port.Allocated() != wxSerialPort::OK)
+  port = new wxSerialPort(portArray[2]);
+  if (port->Allocated() != wxSerialPort::OK)
     std::cerr << "port not allocated" << std::endl;
 
-  r = port.Open(wxSerialPort::MODE_READ_WRITE);
+  r = port->Open(wxSerialPort::MODE_READ_WRITE);
   std::cout << "Open() returned " << static_cast<int>(r) << std::endl;
 
-  r = port.GetConfig(config);
+  r = port->GetConfig(config);
   std::cout << "GetConfig() returned " << static_cast<int>(r) << std::endl;
 
-  if (r == wxSerialPort::OK) r = config.SetBaudrate(110);
+  if (r == wxSerialPort::OK) r = config.SetBaudrate(9600);
   if (r == wxSerialPort::OK) r = config.SetBits(8);
   if (r == wxSerialPort::OK) r = config.SetParity(wxSerialPort::PARITY_NONE);
-  if (r == wxSerialPort::OK) r = config.SetStopbits(2);
+  if (r == wxSerialPort::OK) r = config.SetStopbits(1);
   if (r == wxSerialPort::OK) r = config.SetFlowcontrol(wxSerialPort::FLOWCONTROL_NONE);
 
   std::cout << "config setting calls returned " << static_cast<int>(r) << std::endl;
 
-  r = port.SetConfig(config);
+  r = port->SetConfig(config);
   std::cout << "SetConfig() returned " << static_cast<int>(r) << std::endl;
 
-  wxSerialPort::SignalEventSet *ses;
+  asi = new wxSerialPort::AsyncInput(this, ID_Serial, *port);
+  asi->Start();
+  
+  /*
   r = wxSerialPort::SignalEventSet::New(&ses, this, ID_Serial);
   std::cout << "SignalEventSet::New() returned " << static_cast<int>(r) << std::endl;
 
-  r = ses->AddPortEvent(port, wxSerialPort::EVENT_RX_READY);
+  r = ses->AddPortEvent(*port, wxSerialPort::EVENT_RX_READY);
   std::cout << "AddPortEvent() returned " << static_cast<int>(r) << std::endl;
 
   r = ses->Signal();
   std::cout << "Signal() returned " << static_cast<int>(r) << std::endl;
-
+  */
+  
 #endif
+
+  (void) wxConfigBase::Set(new wxConfig(ROOT_CONFIG));
+
+  wxConfigBase *conf = wxConfigBase::Get();
+
+  if (conf->HasEntry("fred")) {
+    wxString str;
+    (void) conf->Read("fred", &str, "harry");
+    std::cout << "fred = " << str << std::endl;
+  } else {
+    (void) conf->Write("fred", "jim");
+    std::cout << "Initialized fred" << std::endl;
+  }
+}
+
+MyFrame::~MyFrame()
+{
+  /*
+  if (ses) {
+    delete ses;
+    ses = 0;
+  }
+  */
+  if (asi) {
+    asi->Stop();
+    delete asi;
+    asi = 0;
+  }
+
+  if (port) {
+    delete port;
+    port = 0;
+  }
 }
 
 void MyFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
@@ -220,11 +292,34 @@ void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
 
 void MyFrame::Process(unsigned char ch, int source)
 {
-  //std::cout << __PRETTY_FUNCTION__ << std::endl;
-  //asr_ptp->Punch(ch);
+  uint8_t c = ch & 0x7f;
+  port->NonblockingWrite(&c, 1);
 }
 
 void MyFrame::OnSerial(wxSerialPortEvent &event)
 {
-  std::cout << __PRETTY_FUNCTION__ << std::endl;
+  /*
+  uint8_t c;
+  wxSerialPort::Return r;
+  */
+  
+  while (! asi->empty()) {
+    uint8_t c = asi->read();
+    asr_widget->Process(c);
+  }
+
+  /*
+  r = port->NonblockingRead(&c, 1);
+
+  if (r == 1) {
+    asr_widget->Process(c);
+  }
+  
+  if (r >= 0) {
+    // Wait for the next character
+    //r = ses->Signal();
+  }
+  */
+
+  // Is there anyting to do with an error return?
 }
